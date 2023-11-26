d1b00882bcd7c5b7b7c088a36afe20f54e6e5f43
murali-yandra <151438054+murali-yandra@users.noreply.github.com>
2023-11-26 15:14:49 +0530

    Update detect_sklearn_version.py

--- detect_sklearn_version.py
+++ detect_sklearn_version.py
@@ -7,6 +7,6 @@ from sklearn.exceptions import InconsistentVersionWarning
 warnings.simplefilter("error", InconsistentVersionWarning)
 
 try:
-    model = pickle.load(open('saved_model.pkl', 'rb'))
+    model = pickle.load(open('IRIS_Model.pkl', 'rb'))
 except InconsistentVersionWarning as w:
     print(w.original_sklearn_version)
